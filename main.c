0b85547a0b85ebdcfe81c1af148fda34980a8862
Your Name <you@example.com>
2020-01-09 14:27:34 -0500

    Added insertion into one row

--- main.c
+++ main.c
@@ -18,13 +18,56 @@ struct array {
 typedef struct row Row;
 typedef struct array Array;
 
+//Functions for freeing list-related data
+void freeRow(Row *r);
+void freeArray(Array *list);
+
+//Functions for adding data to the lists
+void addElement(Row **r, char element);
+void addRow(Array **list, Row *r);
+
 int main(void) {
 	char c;
+	Array *list;
+	Row *tempRow;
 
 	while (scanf("%c", &c) > 0) {
-		if (c == 10) {
-			printf("Newline");
+		addElement(&tempRow, c);
+	}
+
+	
+
+	freeRow(tempRow);
+}
+
+void freeRow(Row *r) {
+	if (r == NULL) {
+		return;
+	}
+	else {
+		//The print is currently here because it's easier to test
+		printf("%c", r->element);
+		freeRow(r->next);
+		free(r);
+	}
+}
+
+void addElement(Row **r, char element) {
+	//Create the actual data element to be inserted...
+	Row *toInsert = (Row *) malloc(sizeof(Row));
+	toInsert->element = element;
+	toInsert->next = NULL;
+
+	//...Then insert it into the row
+	if (*r == NULL) {
+		*r = toInsert;
+	}
+	else {
+		Row *current = *r;
+		while (current->next != NULL) {
+			current = current->next;
 		}
-		printf("%c", c);
+
+		current->next = toInsert;
 	}
 }
