dfd12562e2994a3fe713757890f3f73751f95d08
Your Name <you@example.com>
2020-01-09 14:13:17 -0500

    Added structs to main, and a bit of info to README

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//A row contains a list of elements, and an array contains a list of rows
+
+struct row {
+	char element;
+
+	struct row * next;
+};
+
+struct array {
+	struct row data;
+
+	struct array * next;
+};
+
+typedef struct row Row;
+typedef struct array Array;
+
 int main(void) {
 
 }
